9a21130431744b4864038d70918ad121a68c1283
Kuba Tarlowski <kuba.tarlowski@gmail.com>
2020-06-22 19:53:16 -0400

    firebase backend for visit/visited using bus_id

--- src/Components/Restaurants/Restaurant/Restaurant.js
+++ src/Components/Restaurants/Restaurant/Restaurant.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Restaurant.module.css';
+import Ratings from 'react-ratings-declarative';
+import icon from '../../../assets/images/food.png'
 
 const restaurant = props => {          
 
@@ -9,14 +11,45 @@ const restaurant = props => {
             return <p key={day}>{day}: {props.hours[day]}</p>
         })
     }
+    
+    let address = props.address;
+    if (address) {
+        address=address+','
+    }
     return (
-        <div className={styles.Restaurant}>
-            <p>{props.name}</p>
-            <p>{props.address}, {props.city}</p>
-            <p>{props.stars}</p>
-            <p>{props.categories}</p>
+        <div 
+            className={styles.Restaurant}>
+            <img className={styles.FoodImage} src={icon} alt={'Food Image'}/>
+            <p><strong style={{fontSize:'15px'}}>{props.name}</strong></p>
+            <p>{address} {props.city}</p>
+            <Ratings 
+                rating={props.stars}
+                widgetRatedColors="red">
+                <Ratings.Widget
+                    widgetDimension="12px"
+                    widgetSpacing="2x"/>
+                <Ratings.Widget
+                    widgetDimension="12px"
+                    widgetSpacing="2px"/>
+                <Ratings.Widget
+                    widgetDimension="12px"
+                    widgetSpacing="2px"/> 
+                <Ratings.Widget
+                    widgetDimension="12px"
+                    widgetSpacing="2px"/>
+                <Ratings.Widget
+                    widgetDimension="12px"
+                    widgetSpacing="2px"/>                               
+            </Ratings>
+            <p style={{color: 'grey'}}>{props.categories}</p>
             <p>{props.phone}</p>
-            {hours}
+            {/* {hours} */}
+            <button 
+                className={styles.Button}
+                onClick={props.visit}>Visit</button>
+            <button 
+                className={styles.Button}
+                onClick={props.visited}>Visited</button>
         </div>
     )
 }
--- src/Components/Restaurants/Restaurants.js
+++ src/Components/Restaurants/Restaurants.js
@@ -2,15 +2,64 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import BusinessDataset from '../../Yelp_Data/sample_businesses.json'
 import Restaurant from './Restaurant/Restaurant'
-
+import firebase from '../../axios-restaurants';
+import styles from './Restaurants.module.css'
 const Restaurants = () => {
     
     const [businesses, setBusinesses] = useState([])
     const [loading,setLoading] = useState(false)
 
+    const deleteBusiness = (index) => {
+        const newBus = [...businesses];
+        newBus.splice(index, 1);
+        setBusinesses(newBus)
+    }
+
+    const updateVisited = (newBus,index) => {
+        deleteBusiness(index)
+        firebase.post('/visited.json',{id: newBus.business_id})
+            .catch(err => console.log(err))
+    }
+
+    const updateVisit = (newBus,index) => {
+        deleteBusiness(index)
+        firebase.post('/visit.json',{id:newBus.business_id})
+            .catch(err => console.log(err))
+    }
+
     const getRestaurants = async () => {
         setLoading(true);
-        await axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search', {
+
+        let myVisit = [];
+        let myVisited = [];
+        await axios.get('https://local-byte.firebaseio.com/visited.json')
+        .then(res => {
+            const tmpVisited = Object.keys(res.data)
+            const visitedIds = tmpVisited.map((key) => {
+                return res.data[key].id
+            })
+            myVisited = [...visitedIds];
+        }).catch(err => {
+            console.log(err)
+        })
+        await axios.get('https://local-byte.firebaseio.com/visit.json')
+        .then(res => {
+            const tmpVisit = Object.keys(res.data)
+            const visitIds = tmpVisit.map((key) => {
+                return res.data[key].id
+            })
+            myVisit = [...visitIds];
+        }).catch(err => {
+            console.log(err)
+        })
+        if (BusinessDataset){
+            const myRest = myVisit.concat(myVisited)
+            const homeRest = BusinessDataset.filter( ( el ) => !myRest.includes( el.business_id ) );
+            setBusinesses(homeRest)
+            setLoading(false)
+            return
+        }
+        await axios.get('https://api.yelp.com/v3/businesses/search', {
             headers: {
 
                 Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
@@ -33,18 +82,20 @@ const Restaurants = () => {
     };
 
     useEffect(() => {
-        setBusinesses(BusinessDataset)
-        // getRestaurants();
+        getRestaurants();        
     }, []);
           
     //rating, price, phone, categories, name
-    const returnedRes = businesses.map((business) => {
+
+    const returnedRes = businesses.map((business, index) => {
         return <Restaurant 
+            className = {styles.Restaurants}
+            visit={() => updateVisit(business,index)}
+            visited={() => updateVisited(business,index)}
             key={business.business_id}
             name={business.name}
             address={business.address}
             city={business.city}
-            state={business.state}
             stars={business.stars}
             categories={business.categories}
             phone={business.display_phone}
